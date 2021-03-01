c2393918b134c0a63f7dea5a4a4d1b217bdbd714
minho0315 <dlalsgh369@naver.com>
2021-03-02 01:18:32 +0900

    useRef in component

--- src/UserList.js
+++ src/UserList.js
@@ -8,28 +8,12 @@ function User({user}) {
     );
 }
 
-function UserList() {
-    const users = [
-        {
-            id: 1,
-            username: 'velopert',
-            email: 'public.velopert@gmail.com'
-        },
-        {
-            id: 2,
-            username: 'tester',
-            email: 'test@example.com'
-        },
-        {
-            id: 3,
-            username: 'liz',
-            email: 'liz@example.com'
-        }
-    ];
+function UserList({users}) {
+    
     return (
         <div>
-            {users.map((user, index) => (
-                <User user={user} key={index} />
+            {users.map(user => (
+                <User user={user} key={user.id} />
             ))}
         </div>
     );
