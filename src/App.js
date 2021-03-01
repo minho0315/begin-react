d807a5a7f3f71e22b19b45785ecba9bf2bb3fa46
minho0315 <dlalsgh369@naver.com>
2021-03-02 01:41:13 +0900

    array item delete

--- src/App.js
+++ src/App.js
@@ -1,54 +1,60 @@
-import React, {useRef, useState} from 'react';
-import UserList from './UserList';
-import CreateUser from './CreateUser';
+import React, { useRef, useState } from "react";
+import UserList from "./UserList";
+import CreateUser from "./CreateUser";
 
 function App() {
   const [inputs, setInputs] = useState({
-    username:'',
-    email:''
+    username: "",
+    email: "",
   });
-  const {username, email} = inputs;
-  const onChange = e => {
-    const {name, value} = e.target;
+  const { username, email } = inputs;
+  const onChange = (e) => {
+    const { name, value } = e.target;
     setInputs({
       ...inputs,
-      [name]: value
+      [name]: value,
     });
   };
 
-  const [users,setUsers] = useState([
+  const [users, setUsers] = useState([
     {
-        id: 1,
-        username: 'velopert',
-        email: 'public.velopert@gmail.com'
+      id: 1,
+      username: "velopert",
+      email: "public.velopert@gmail.com",
     },
     {
-        id: 2,
-        username: 'tester',
-        email: 'test@example.com'
+      id: 2,
+      username: "tester",
+      email: "test@example.com",
     },
     {
-        id: 3,
-        username: 'liz',
-        email: 'liz@example.com'
-    }
-]);
+      id: 3,
+      username: "liz",
+      email: "liz@example.com",
+    },
+  ]);
 
-const nextId = useRef(4);
-const onCreate = () => {
-  const user = {
-    id: nextId.current,
-    username,
-    email
+  const nextId = useRef(4);
+  const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    };
+    setUsers(users.concat(user));
+    //setUsers([...user,user]);
+    setInputs({
+      username: "",
+      email: "",
+    });
+    nextId.current += 1;
   };
-  setUsers(users.concat(user));
-  //setUsers([...user,user]);
-  setInputs({
-    username: '',
-    email: ''
-  });
-  nextId.current += 1;
-};
+
+  const onRemove = (id) => {
+    // user.id가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
+    // =user.id 가 id인 것을 제거함
+    setUsers(users.filter((user) => user.id !== id));
+  };
   return (
     <>
       <CreateUser
@@ -57,7 +63,7 @@ const onCreate = () => {
         onChange={onChange}
         onCreate={onCreate}
       />
-      <UserList users={users} />
+      <UserList users={users} onRemove={onRemove} />
     </>
   );
 }
