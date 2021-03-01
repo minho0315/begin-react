216c9bc2df46abbd07baeedfc9dbd78ac5bfe07d
minho0315 <dlalsgh369@naver.com>
2021-03-02 00:54:26 +0900

    useRef

--- src/InputSample.js
+++ src/InputSample.js
@@ -1,10 +1,11 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 
 function InputSample() {
     const [inputs, setInputs] = useState({
         name: '',
         nickname: ''
     });
+    const nameInput = useRef();
 
     const {name, nickname} = inputs; //비구조화 할당을 통해 값 추출
 
@@ -21,18 +22,30 @@ function InputSample() {
             name: '',
             nickname: '',
         })
+        nameInput.current.focus();
     };
 
     return (
+      <div>
+        <input
+          name="name"
+          placeholder="이름"
+          onChange={onChange}
+          value={name}
+          ref={nameInput}
+        />
+        <input
+          name="nickname"
+          placeholder="닉네임"
+          onChange={onChange}
+          value={nickname}
+        />
+        <button onClick={onReset}>초기화</button>
         <div>
-            <input name="name" placeholder="이름" onChange={onChange} value={name} />
-            <input name="nickname" placeholder="닉네임" onChange={onChange} value={nickname} />
-            <button onClick={onReset}>초기화</button>
-            <div>
-                <b>값: </b>
-                {name} ({nickname})
-            </div>
+          <b>값: </b>
+          {name} ({nickname})
         </div>
+      </div>
     );
 }
 
