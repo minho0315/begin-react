a3198555db3ed480065780c1b26695011eda398f
minho0315 <dlalsgh369@naver.com>
2021-03-02 20:33:39 +0900

    useEffect

--- src/UserList.js
+++ src/UserList.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useEffect} from 'react';
 
 function User({user, onRemove, onToggle}) {
+    useEffect(() => {
+            console.log(user);
+        });
     return (
         <div>
             <b
@@ -12,7 +15,7 @@ function User({user, onRemove, onToggle}) {
             >
                 {user.username}
             </b> 
-            
+            &nbsp
             <span>({user.email})</span>
             <button onClick={()=> onRemove(user.id)}>삭제</button>
         </div>
